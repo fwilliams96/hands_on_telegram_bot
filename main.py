8990954ba017d40afcec1d53cd5471205665c795
Francisco <fwmcomputer@gmail.com>
2025-02-02 01:44:24 +0100

    Create webhook endpoint

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-def main():
-    print("Hello World")
+from fastapi import FastAPI, Request
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+@app.post("/webhook")
+async def webhook(request: Request):
+    try:
+        data = await request.json()
+        chat_id = data["message"]["chat"]["id"]
+        user_message = data["message"]["text"]
+
+        print(f"Received message: {user_message}")
+        print(f"Chat ID: {chat_id}")
+
+        return {"status": "success", "message": "Message handled successfully"}
+    except Exception as e:
+        print(f"Error handling message: {e}")
+        return {"status": "error", "message": "Error handling message"}
