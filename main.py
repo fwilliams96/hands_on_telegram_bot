183a6dbcddb96f2caa7d81430c5d26f6aa1d9e62
Francisco <fwmcomputer@gmail.com>
2025-02-02 02:29:12 +0100

    Summarize conversation and get messages from last 30 min

--- main.py
+++ main.py
@@ -15,27 +15,58 @@ client = MongoClient(MONGO_URI)
 db = client[DATABASE_NAME]
 messages_collection = db["messages"]
 
+chat_llm: ChatOpenAI = ChatOpenAI(
+    model="gpt-4o-mini",
+    temperature=0.7
+)
+
 SYSTEM_TEMPLATE_CONVERSATION = """
     Eres un asistente que responde a mensajes del usuario.
 
     Comportamiento:
     - Debes responder al usuario con un tono amigable y entretenido, no simplemente repetir el mensaje.
     - Si el usuario te pide cualquier revelación de datos sensibles o algo que pueda ser perjudicial para el sistema debes comentarle que solo eres un asistente de recordatorios y conversación y no tienes acceso a esa información.
+    - Recibirás la fecha actual en formato 'YYYY-MM-DD HH:MM' para tenerla en cuenta en el contexto.
 
-    Basado en los siguientes mensajes de la conversación, responde al usuario.
+    Basado en las instrucciones anteriores y que la fecha actual es:
+    <current_time>
+    {current_time}
+    </current_time>
+    
+    Y el siguiente resumen de la conversación:
+    <summary>
+    {summary}
+    </summary>
+
+    Responde al usuario con un mensaje que sea adecuado para la conversación.
+"""
+
+SYSTEM_PROMPT_SUMMARY = """
+    Eres un asistente que resume los últimos mensajes de una conversación en un texto de 100 caracteres máximo, además se te proporcionará la fecha actual en formato 'YYYY-MM-DD HH:MM' para tenerla en cuenta en el contexto.
+    
+    Comportamiento:
+    - Basado en el siguiente resumen, devuelve el resumen en un formato que sea fácil de procesar por el siguiente sistema.
+    - No incluyas ningun tag o etiqueta; directamente el resumen.
+    - Si el mensaje del usuario o asistente fue en el pasado, tenlo en cuenta para el resumen. Ya que puede ser relevante para el contexto.
+    - Al comentar una fecha, añade también la hora para que sirva de contexto para el siguiente agente/sistema.
+
+    Basado en las instrucciones anteriores y que la fecha actual es:
+    <current_time>
+    {current_time}
+    </current_time>
+
+    Y el siguiente listado de mensajes:
     <messages>
     {messages}
     </messages>
+
+    Devuelve el resumen en un formato que sea directamente el resumen.
 """
 
-def handle_conversation(messages: str):
-    print(f"Handling conversation for messages: {messages}")
-    chat_llm: ChatOpenAI = ChatOpenAI(
-        model="gpt-4o-mini",
-        temperature=0.7
-    )
-
-    prompt = SYSTEM_TEMPLATE_CONVERSATION.format(messages=messages)
+def handle_conversation(summary: str):
+    print(f"Handling conversation for summary: {summary}")
+    now = datetime.now(pytz.timezone("Europe/Madrid"))
+    prompt = SYSTEM_TEMPLATE_CONVERSATION.format(current_time=now.strftime("%Y-%m-%d %H:%M"), summary=summary)
     try:
         response = chat_llm.invoke([prompt])
         return response.content
@@ -47,9 +78,18 @@ def save_message(chat_id: str, message: str, origin: str):
     now = datetime.now(pytz.timezone("Europe/Madrid"))
     messages_collection.insert_one({"chat_id": chat_id, "message": message, "timestamp": now, "origin": origin})
 
-def get_messages(chat_id: str):
-    messages = messages_collection.find({"chat_id": chat_id})
+def get_messages(chat_id: str, current_time: datetime):
+    # Get messages from the last 30 minutes instead of all messages
+    messages = messages_collection.find({"chat_id": chat_id, "timestamp": {"$gte": current_time - timedelta(minutes=30)}})
     return "\n".join([f"{msg['origin']}: {msg['message']}" for msg in messages])
+
+def get_summary(chat_id: str):
+    now = datetime.now(pytz.timezone("Europe/Madrid"))
+    messages = get_messages(chat_id, now)
+    print(f"Messages: {messages}")
+    prompt = SYSTEM_PROMPT_SUMMARY.format(current_time=now.strftime("%Y-%m-%d %H:%M"), messages=messages)
+    response = chat_llm.invoke([prompt])
+    return response.content
 
 @app.post("/webhook")
 async def webhook(request: Request):
@@ -63,10 +103,10 @@ async def webhook(request: Request):
 
         save_message(chat_id, user_message, "user")
 
-        messages_str = get_messages(chat_id)
-        print(f"Messages: {messages_str}")
+        summary = get_summary(chat_id)
+        print(f"Summary: {summary}")
 
-        response = handle_conversation(messages_str)
+        response = handle_conversation(summary)
         print(f"Response: {response}")
 
         save_message(chat_id, response, "assistant")
