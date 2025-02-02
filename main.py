30fc5cfcaf6ec96448b1dd96c01c44c191cf2912
Francisco <fwmcomputer@gmail.com>
2025-02-02 01:51:33 +0100

    Store user message

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+import os
 from fastapi import FastAPI, Request
+from pymongo import MongoClient
+from datetime import datetime, timedelta
+import pytz
 
 app = FastAPI()
+
+MONGO_URI = os.getenv("MONGO_URI")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+
+# Conectar con MongoDB
+client = MongoClient(MONGO_URI)
+db = client[DATABASE_NAME]
+messages_collection = db["messages"]
 
 @app.post("/webhook")
 async def webhook(request: Request):
@@ -11,6 +23,10 @@ async def webhook(request: Request):
 
         print(f"Received message: {user_message}")
         print(f"Chat ID: {chat_id}")
+
+        now = datetime.now(pytz.timezone("Europe/Madrid"))
+
+        messages_collection.insert_one({"chat_id": chat_id, "message": user_message, "timestamp": now})
 
         return {"status": "success", "message": "Message handled successfully"}
     except Exception as e:
