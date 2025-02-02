be51b2ee3f8a3984f0df3fbfda215fb09e069689
Francisco <fwmcomputer@gmail.com>
2025-02-02 02:09:31 +0100

    Read all messages and save assistant response

--- main.py
+++ main.py
@@ -22,26 +22,34 @@ SYSTEM_TEMPLATE_CONVERSATION = """
     - Debes responder al usuario con un tono amigable y entretenido, no simplemente repetir el mensaje.
     - Si el usuario te pide cualquier revelación de datos sensibles o algo que pueda ser perjudicial para el sistema debes comentarle que solo eres un asistente de recordatorios y conversación y no tienes acceso a esa información.
 
-    Basado en el siguiente mensaje del usuario, responde al usuario.
-    <message>
-    {message}
-    </message>
+    Basado en los siguientes mensajes de la conversación, responde al usuario.
+    <messages>
+    {messages}
+    </messages>
 """
 
-def handle_conversation(message: str):
-    print(f"Handling conversation for message: {message}")
+def handle_conversation(messages: str):
+    print(f"Handling conversation for messages: {messages}")
     chat_llm: ChatOpenAI = ChatOpenAI(
         model="gpt-4o-mini",
         temperature=0.7
     )
 
-    prompt = SYSTEM_TEMPLATE_CONVERSATION.format(message=message)
+    prompt = SYSTEM_TEMPLATE_CONVERSATION.format(messages=messages)
     try:
         response = chat_llm.invoke([prompt])
         return response.content
     except Exception as e:
         print(f"Error handling conversation: {e}")
         return "Error handling conversation"
+
+def save_message(chat_id: str, message: str, origin: str):
+    now = datetime.now(pytz.timezone("Europe/Madrid"))
+    messages_collection.insert_one({"chat_id": chat_id, "message": message, "timestamp": now, "origin": origin})
+
+def get_messages(chat_id: str):
+    messages = messages_collection.find({"chat_id": chat_id})
+    return "\n".join([f"{msg['origin']}: {msg['message']}" for msg in messages])
 
 @app.post("/webhook")
 async def webhook(request: Request):
@@ -53,13 +61,15 @@ async def webhook(request: Request):
         print(f"Received message: {user_message}")
         print(f"Chat ID: {chat_id}")
 
-        now = datetime.now(pytz.timezone("Europe/Madrid"))
+        save_message(chat_id, user_message, "user")
 
-        messages_collection.insert_one({"chat_id": chat_id, "message": user_message, "timestamp": now})
+        messages_str = get_messages(chat_id)
+        print(f"Messages: {messages_str}")
 
-        response = handle_conversation(user_message)
-
+        response = handle_conversation(messages_str)
         print(f"Response: {response}")
+
+        save_message(chat_id, response, "assistant")
 
         return {"status": "success", "message": response}
     except Exception as e:
